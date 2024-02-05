3ef103de08e4be77b7525655384cb3a11bd43b0d
ADiBog <dronbog@gmail.com>
2024-02-05 21:11:15 +0300

    1st commit

---
+++ src/main/java/com/example/banksystem/BankSystemApplication.java
@@ -0,0 +1,16 @@
+package com.example.banksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Сервис запуска приложения.
+ */
+@SpringBootApplication
+public class BankSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSystemApplication.class, args);
+    }
+
+}
